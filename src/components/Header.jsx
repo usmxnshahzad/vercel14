b3bea660bc1a108ddd9d39008e0e5b7b5a233a86
usmxnshahzad <usman@example.com>
2026-03-16 12:09:07 +0500

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,36 @@
+"use client"
+
+import React from 'react'
+import Link from 'next/link'
+import { SignInButton, UserButton, Show } from "@clerk/nextjs"
+
+function Header() {
+  return (
+    <header className='bg-gradient-to-r from-blue-300 to-purple-400 shadow-lg'>
+      <div className="max-w-6xl mx-auto flex justify-between items-center p-3"> 
+        <Link href="/" className="text-2xl font-extrabold group cursor-pointer">
+          <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-blue-700 drop-shadow-md group-hover:from-blue-400 group-hover:to-blue-600">
+            Auth
+          </span>
+          <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-purple-700 drop-shadow-md group-hover:from-purple-400 group-hover:to-purple-600">
+            App
+          </span>
+        </Link>
+        <nav>
+          <ul className='flex gap-4 items-center'>
+            <li><Link href="/">Home</Link></li>
+            <li><Link href="/about">About</Link></li>
+            <Show when="signed-in">
+              <li><UserButton /></li>
+            </Show>
+            <Show when="signed-out">
+              <li><SignInButton /></li>
+            </Show>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  )
+}
+
+export default Header
