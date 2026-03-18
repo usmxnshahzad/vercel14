2f5fface62cb62fde992b96a328748fa893da8e4
usmxnshahzad <usman@example.com>
2026-03-18 09:43:53 +0500

    fix middleware public routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,6 +2,8 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
 const isPublicRoute = createRouteMatcher([
   '/',
+  '/sign-in(.*)',
+  '/sign-up(.*)',
   '/forget-password',
   '/api/webhooks/clerk'
 ])
